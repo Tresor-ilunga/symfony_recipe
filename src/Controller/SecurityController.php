c8d4147b69f73871da7710a6b63e2abf52dcf2bf
Tresor-ilunga <19im065@esisalama.org>
2022-11-10 21:49:52 +0100

    🚧 Setting up login form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'security.logout', methods: ['GET', 'POST'])]
+    public function logout()
+    {
+        // Nothing to do here..
+    }
+}
