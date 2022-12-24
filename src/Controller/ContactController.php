9dceaeecc6cf029e28bcbc070eb636d2e81457f3
Tresor-ilunga <19im065@esisalama.org>
2022-12-24 22:14:08 +0100

    Sending emails

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/contact', name: 'contact.index', methods: ['GET', 'POST'])]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
@@ -31,8 +38,20 @@ class ContactController extends AbstractController
             $contact = $form->getData();
 
             $manager->persist($contact);
-
             $manager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('admin@recipe.com')
+                ->subject($contact->getSubject())
+                ->htmlTemplate('emails/contact.html.twig')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'contact' => $contact
+                ]);
+
+            $mailer->send($email);
 
             $this->addFlash
             (
