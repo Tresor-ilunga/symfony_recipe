3de1dbece08fc86113696beaf4e27c48ee9d20db
Tresor-ilunga <19im065@esisalama.org>
2022-12-31 19:28:08 +0100

    🔥 Implementation of tests

---
+++ tests/Functional/BasicTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BasicTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
